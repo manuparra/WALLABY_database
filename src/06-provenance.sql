4939aa61af6180f083d38ba5206879f80922979b
Austin Shen <austin.shen@csiro.au>
2022-01-04 10:21:11 +0800

    tag for source_detection table to set release information

--- src/06-provenance.sql
+++ src/06-provenance.sql
@@ -1,7 +1,6 @@
--- Add pipeline execution provenance metadata to wallabydb
-
 \connect wallabydb
 
+-- Add pipeline execution provenance metadata to wallabydb
 CREATE TABLE wallaby.run_metadata (
     "id" BIGSERIAL PRIMARY KEY,
     "run_id" BIGINT NOT NULL,
@@ -12,5 +11,15 @@ CREATE TABLE wallaby.run_metadata (
     "parameters" jsonb NOT NULL,
     "datetime" timestamp without time zone NOT NULL
 );
-
 ALTER TABLE wallaby.run_metadata ADD FOREIGN KEY ("run_id") REFERENCES wallaby.run ("id") ON DELETE CASCADE;
+
+-- Tag source detection for release data
+CREATE TABLE wallaby.tag_source_detection (
+  "id" BIGSERIAL PRIMARY KEY,
+  "tag_id" bigint NOT NULL,
+  "source_detection_id" bigint NOT NULL,
+  "author" text NOT NULL,
+  "added_at" timestamp without time zone NOT NULL
+);
+ALTER TABLE wallaby.tag_source_detection ADD FOREIGN KEY ("tag_id") REFERENCES wallaby.tag ("id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE wallaby.tag_source_detection ADD FOREIGN KEY ("source_detection_id") REFERENCES wallaby.source_detection ("id") ON UPDATE CASCADE ON DELETE CASCADE;
