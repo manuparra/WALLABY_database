2c4ec0edffa0dd491f5ac24863bc49ac43fdd1d1
Austin Shen <austin.shen@csiro.au>
2022-02-17 08:56:27 +0000

    permissions

--- src/02-source-finding.sql
+++ src/02-source-finding.sql
@@ -155,3 +155,31 @@ ALTER TABLE wallaby.tag_detection ADD COLUMN "added_at" timestamp without time z
 ALTER TABLE wallaby.tag_detection ALTER COLUMN added_at SET DEFAULT now();
 ALTER TABLE wallaby.tag_detection ADD FOREIGN KEY ("tag_id") REFERENCES wallaby.tag ("id") ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE wallaby.tag_detection ADD FOREIGN KEY ("detection_id") REFERENCES wallaby.detection ("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+-- Tag source detection for release data
+CREATE TABLE wallaby.tag_source_detection (
+  "id" BIGSERIAL PRIMARY KEY
+);
+ALTER TABLE wallaby.tag_source_detection ADD COLUMN "tag_id" bigint NOT NULL;
+ALTER TABLE wallaby.tag_source_detection ADD COLUMN "source_detection_id" bigint NOT NULL;
+ALTER TABLE wallaby.tag_source_detection ADD COLUMN "author" text NOT NULL;
+ALTER TABLE wallaby.tag_source_detection ADD COLUMN "added_at" timestamp without time zone;
+ALTER TABLE wallaby.tag_source_detection ALTER COLUMN added_at SET DEFAULT now();
+ALTER TABLE wallaby.tag_source_detection ADD FOREIGN KEY ("tag_id") REFERENCES wallaby.tag ("id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE wallaby.tag_source_detection ADD FOREIGN KEY ("source_detection_id") REFERENCES wallaby.source_detection ("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+-- Add pipeline execution provenance metadata to wallabydb
+CREATE TABLE wallaby.run_metadata (
+    "id" BIGSERIAL PRIMARY KEY
+);
+ALTER TABLE wallaby.run_metadata ADD COLUMN "run_id" BIGINT NOT NULL;
+ALTER TABLE wallaby.run_metadata ADD COLUMN "repository" varchar NOT NULL;
+ALTER TABLE wallaby.run_metadata ADD COLUMN "branch" varchar NOT NULL;
+ALTER TABLE wallaby.run_metadata ADD COLUMN "version" varchar NOT NULL;
+ALTER TABLE wallaby.run_metadata ADD COLUMN "configuration" jsonb NOT NULL;
+ALTER TABLE wallaby.run_metadata ADD COLUMN "parameters" jsonb NOT NULL;
+ALTER TABLE wallaby.run_metadata ADD COLUMN "added_at" timestamp without time zone;
+ALTER TABLE wallaby.run_metadata ALTER COLUMN added_at SET DEFAULT now();
+ALTER TABLE wallaby.run_metadata ADD FOREIGN KEY ("run_id") REFERENCES wallaby.run ("id") ON DELETE CASCADE;
--- src/05-privileges.sql
+++ src/05-privileges.sql
@@ -10,6 +10,8 @@ ALTER TABLE wallaby.source_detection OWNER TO "admin";
 ALTER TABLE wallaby.tag OWNER TO "admin";
 ALTER TABLE wallaby.tag_detection OWNER TO "admin";
 ALTER TABLE wallaby.comment OWNER TO "admin";
+ALTER TABLE wallaby.tag_source_detection OWNER TO "admin";
+ALTER TABLE wallaby.run_metadata OWNER TO "admin";
 ALTER TABLE wallaby.kinematic_model OWNER TO "admin";
 
 -- Admin privileges
@@ -20,22 +22,22 @@ GRANT ALL PRIVILEGES ON DATABASE wallabydb TO "gavo";
 -- Read-only wallaby user
 GRANT CONNECT ON DATABASE wallabydb TO "wallaby_user";
 GRANT USAGE ON SCHEMA "wallaby" TO "wallaby_user";
-GRANT SELECT ON TABLE wallaby.run, wallaby.instance, wallaby.detection, wallaby.product, wallaby.source, wallaby.source_detection, wallaby.tag, wallaby.tag_detection, wallaby.comment, wallaby.kinematic_model TO "wallaby_user";
+GRANT SELECT ON TABLE wallaby.run, wallaby.instance, wallaby.detection, wallaby.product, wallaby.source, wallaby.source_detection, wallaby.tag, wallaby.tag_detection, wallaby_tag_source_detection, wallaby.comment, wallaby_run_metadata, wallaby.kinematic_model TO "wallaby_user";
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA wallaby TO "wallaby_user";
 
 -- Permissions for VO services
 GRANT CONNECT ON DATABASE wallabydb TO "gavoadmin";
 GRANT USAGE ON SCHEMA "wallaby" TO "gavoadmin";
-GRANT SELECT ON TABLE wallaby.run, wallaby.instance, wallaby.detection, wallaby.product, wallaby.source, wallaby.source_detection, wallaby.tag, wallaby.tag_detection, wallaby.comment, wallaby.kinematic_model TO "gavoadmin";
+GRANT SELECT ON TABLE wallaby.run, wallaby.instance, wallaby.detection, wallaby.product, wallaby.source, wallaby.source_detection, wallaby.tag, wallaby.tag_detection, wallaby_tag_source_detection, wallaby.comment, wallaby_run_metadata, wallaby.kinematic_model TO "gavoadmin";
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA wallaby TO "gavoadmin";
 
 GRANT CONNECT ON DATABASE wallabydb TO "gavo";
 GRANT USAGE ON SCHEMA "wallaby" TO "gavo";
-GRANT SELECT ON TABLE wallaby.run, wallaby.instance, wallaby.detection, wallaby.product, wallaby.source, wallaby.source_detection, wallaby.tag, wallaby.tag_detection, wallaby.comment, wallaby.kinematic_model TO "gavo";
+GRANT SELECT ON TABLE wallaby.run, wallaby.instance, wallaby.detection, wallaby.product, wallaby.source, wallaby.source_detection, wallaby.tag, wallaby.tag_detection, wallaby_tag_source_detection, wallaby.comment, wallaby_run_metadata, wallaby.kinematic_model TO "gavo";
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA wallaby TO "gavo";
 
 -- Other
 GRANT CONNECT ON DATABASE wallabydb TO "untrusted";
 GRANT USAGE ON SCHEMA "wallaby" TO "untrusted";
-GRANT SELECT ON TABLE wallaby.run, wallaby.instance, wallaby.detection, wallaby.product, wallaby.source, wallaby.source_detection, wallaby.tag, wallaby.tag_detection, wallaby.comment, wallaby.kinematic_model TO "untrusted";
+GRANT SELECT ON TABLE wallaby.run, wallaby.instance, wallaby.detection, wallaby.product, wallaby.source, wallaby.source_detection, wallaby.tag, wallaby.tag_detection, wallaby_tag_source_detection, wallaby.comment, wallaby_run_metadata, wallaby.kinematic_model TO "untrusted";
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA wallaby TO "untrusted";
